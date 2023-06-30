64f6479e61635492b06430eaaab08e83b87f8b1a
Lewis <shuaicode@gmail.com>
2023-06-30 14:05:14 +0800

    fix role

--- controller/role.go
+++ controller/role.go
@@ -35,3 +35,78 @@ func (c *RoleController) GetRoleList(ctx *app.Context) {
 
 	ctx.JSONSuccess(roles)
 }
+
+// 创建角色
+// @Summary 创建角色
+// @Tags 角色
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "token"
+// @Param params body model.Role true "角色信息"
+// @Success 200 {object} model.Role
+// @Router /role/create [post]
+func (c *RoleController) CreateRole(ctx *app.Context) {
+	param := &model.Role{}
+	if err := ctx.ShouldBindJSON(param); err != nil {
+		ctx.JSONError(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	err := c.RoleService.CreateRole(ctx, param)
+	if err != nil {
+		ctx.JSONError(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx.JSONSuccess(param)
+}
+
+// 更新角色
+// @Summary 更新角色
+// @Tags 角色
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "token"
+// @Param params body model.Role true "角色信息"
+// @Success 200 {object} app.Response "Successfully updated role"
+// @Router /role/update [post]
+func (c *RoleController) UpdateRole(ctx *app.Context) {
+	param := &model.Role{}
+	if err := ctx.ShouldBindJSON(param); err != nil {
+		ctx.JSONError(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	err := c.RoleService.UpdateRole(ctx, param)
+	if err != nil {
+		ctx.JSONError(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx.JSONSuccess("更新角色成功")
+}
+
+// 删除角色
+// @Summary 删除角色
+// @Tags 角色
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "token"
+// @Param params body model.Role true "角色ID"
+// @Success 200 {object} app.Response "Successfully deleted role"
+// @Router /role/delete [post]
+func (c *RoleController) DeleteRole(ctx *app.Context) {
+	param := &model.Role{}
+	if err := ctx.ShouldBindJSON(param); err != nil {
+		ctx.JSONError(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	err := c.RoleService.DeleteRole(ctx, param.Uuid)
+	if err != nil {
+		ctx.JSONError(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx.JSONSuccess("删除角色成功")
+}
